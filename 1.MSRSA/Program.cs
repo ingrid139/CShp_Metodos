067cfdec24aba96597e0d29afe090901724a8643
ingrid139 <ingrid139@hotmail.com>
2020-10-22 14:35:06 -0300

    Add project files.

---
+++ 1.MSRSA/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _1.MSRSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 1 - Metodo sem retorno sem argumento");
+
+            //Exemplo
+            Alisa();
+
+            //Exercicio
+            Contador();
+
+            Console.ReadKey();
+        }
+
+        public static void Alisa()
+        {
+            Console.WriteLine("Pegar prancha");
+            Console.WriteLine("Ligar tomada");
+            Console.WriteLine("Aguardar esquentar");
+            Console.WriteLine("4 - Separar a mecha a ser alisada");
+            Console.WriteLine("5 - Passar a prancha na mecha separada");
+            Console.WriteLine("Repetir 4 e 5 até que todo o cabelo esteja liso");
+        }
+
+        public static void Contador()
+        {
+            Console.WriteLine("Olá, sou o Metodo sem retorno sem argumento");
+
+            for (int contador = 1; contador <= 10; contador++)
+            {
+                Console.WriteLine(contador);
+            }
+        }
+    }
+}
---
+++ MCRCA/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace MCRCA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 3 - Metodo com retorno sem argumento");
+
+            //Exemplo
+            Notas notas = BuscaNotas();
+            CalculaMedia(notas.n1, notas.n2);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Exercicio
+            int valor = BuscaLimite();
+
+            Contador(valor);
+
+            Console.ReadKey();
+        }
+
+        public static Notas BuscaNotas()
+        {
+            Notas notas = new Notas();
+
+            Console.WriteLine("Informe a nota 1");
+
+            string n1Digitada = Console.ReadLine();
+            notas.n1 = Int32.Parse(n1Digitada);
+
+            Console.WriteLine("Informe a nota 2");
+
+            string n2Digitada = Console.ReadLine();
+            notas.n2 = Int32.Parse(n2Digitada);
+
+            return notas;
+        }
+
+        public static int BuscaLimite()
+        {
+            Console.WriteLine("Olá, sou o Metodo com retorno sem argumento");
+
+            Console.WriteLine("Digite um valor para dar início a contagem");
+            string valorDigitado = Console.ReadLine();
+            int valor = Int32.Parse(valorDigitado);
+            return valor;
+        }
+
+        public static void CalculaMedia(int n1, int n2)
+        {
+            double media = (n1 + n2) / 2;
+
+            Console.WriteLine("O média é: " + media);
+        }
+
+        public static void Contador(int limite)
+        {
+            Console.WriteLine("Olá, sou o Metodo sem retorno com argumento");
+
+            for (int contador = 1; contador <= limite; contador++)
+            {
+                Console.WriteLine(contador);
+            }
+        }
+    }
+
+    public class Notas
+    {
+        public int n1 { get; set; }
+        public int n2 { get; set; }
+    }
+}
